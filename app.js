7dfc783dfaa66b572dd594f08cbdc2df95761994
koleLunger <55799232+koleLunger@users.noreply.github.com>
2020-01-23 14:19:28 -0500

    fixing sql

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 var sqlite3 = require('sqlite3').verbose();
 var db = new sqlite3.Database('database.db');
-db.run("CREATE TABLE lorem (something else TEXT)");
+db.run("CREATE TABLE lorem (something TEXT)");
