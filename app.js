4f72bd5b76dcb519d7e5fd848b1fb29cc7b08d8d
koleLunger <55799232+koleLunger@users.noreply.github.com>
2020-01-23 14:45:30 -0500

    sql fixed

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
 var sqlite3 = require('sqlite3').verbose();
 var db = new sqlite3.Database('database.db');
-db.run("CREATE TABLE lorem (something TEXT)");
+//db.run("CREATE TABLE lorem (something TEXT)");
+let value = 'pikachu';
+db.run("INSERT INTO lorem (something) VALUES (\"" + value + "\")")
